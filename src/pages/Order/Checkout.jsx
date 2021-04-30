7e02ae06755917b4bd09bff20fa148128098dca9
kayacchang <egg734631@gmail.com>
2021-04-30 13:27:34 +0800

    refactor

--- src/pages/Order/Checkout.jsx
+++ src/pages/Order/Checkout.jsx
@@ -1,26 +1,52 @@
+import { useHistory } from "react-router-dom";
 import clsx from "clsx";
 
-export default function Checkout({ total, count }) {
+export default function Checkout({ orders, products }) {
+  const history = useHistory();
+
+  const { total, count } = Object.entries(orders).reduce(
+    ({ total, count }, [name, orders]) => {
+      const { price } = products.find((product) => product.name === name);
+
+      return {
+        total: total + orders.length * price,
+        count: count + orders.length,
+      };
+    },
+    { total: 0, count: 0 }
+  );
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    history.push("/checkout");
+  }
+
+  if (count <= 0) return <></>;
+
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex items-end ">
-        <span className="text-4xl">${total}</span>
+    <div className="text-on-primary border-t-8 border-primary px-8 py-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-end ">
+          <span className="text-4xl">${total}</span>
 
-        <div className="pb-1">
-          /{count} CUP{count > 1 && "S"}
+          <div className="pb-1">
+            /{count} CUP{count > 1 && "S"}
+          </div>
         </div>
-      </div>
 
-      <button
-        type="submit"
-        className={clsx(
-          "flex flex-col items-center bg-primary border border-on-primary px-4 py-2 ml-auto",
-          count || "pointer-events-none"
-        )}
-      >
-        <span className="text-xl">PLACE</span>
-        <span>the order</span>
-      </button>
+        <button
+          type="submit"
+          className={clsx(
+            "flex flex-col items-center bg-primary border border-on-primary px-4 py-2 ml-auto",
+            count || "pointer-events-none"
+          )}
+          onClick={onSubmit}
+        >
+          <span className="text-xl">PLACE</span>
+          <span>the order</span>
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -9,28 +9,9 @@ import Checkout from "./Checkout";
 import { useProducts } from "../../contexts/products";
 
 export default function Order() {
-  const history = useHistory();
   const { product } = useParams();
   const products = useProducts();
   const [orders, setOrders] = useState({});
-
-  const { total, count } = Object.entries(orders).reduce(
-    ({ total, count }, [name, orders]) => {
-      const { price } = products.find((product) => product.name === name);
-
-      return {
-        total: total + orders.length * price,
-        count: count + orders.length,
-      };
-    },
-    { total: 0, count: 0 }
-  );
-
-  function onSubmit(event) {
-    event.preventDefault();
-
-    history.push("/checkout");
-  }
 
   if (products.length <= 0) {
     return <Layout></Layout>;
@@ -38,7 +19,10 @@ export default function Order() {
 
   return (
     <Layout>
-      <form onSubmit={onSubmit} className="flex flex-col">
+      <form
+        onSubmit={(event) => event.preventDefault()}
+        className="flex flex-col"
+      >
         <header className="bg-background z-10">
           <div className="bg-primary text-on-primary pt-1 pb-2 px-2">
             <Tabs types={[...new Set(products.map(({ type }) => type))]} />
@@ -55,11 +39,9 @@ export default function Order() {
           <Orders orders={orders} setOrders={setOrders} />
         </main>
 
-        {count > 0 && (
-          <footer className="bg-background text-on-primary border-t-8 border-primary px-8 py-4">
-            <Checkout total={total} count={count} />
-          </footer>
-        )}
+        <footer>
+          <Checkout orders={orders} products={products} />
+        </footer>
       </form>
     </Layout>
   );
